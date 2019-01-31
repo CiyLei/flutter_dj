f3d75723732519f5fd87eff2ae6d24c3fca5d527
cl <903736662@qq.com>
2019-01-31 13:47:49 +0800

    opt: 修改main启动入口

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_demo/dj/djoa.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new DJOA());
 
 class MyApp extends StatelessWidget {
 
